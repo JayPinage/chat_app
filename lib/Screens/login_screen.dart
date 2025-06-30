ad7acee256376e4d6a82367ff52f1fc3ddacc6d7
JAY PINAGE <bscit.jaypinage@gmail.com>
2025-06-30 12:29:18 +0530

    signup update

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -28,90 +28,93 @@ class _LoginScreen extends State<LoginScreen> {
 
           return state is AuthLoadingState ?Center(child: CircularProgressIndicator(),): Form(
             key: user,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Login", style: TextStyle(fontSize: 20)),
-
-                SizedBox(height: 10),
-
-                TextFormField(
-                  controller: email,
-
-                  decoration: InputDecoration(
-                    hintText: "Enter Email",
-
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: Colors.deepOrange),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Login", style: TextStyle(fontSize: 20)),
+              
+                  SizedBox(height: 10),
+              
+                  TextFormField(
+                    controller: email,
+              
+                    decoration: InputDecoration(
+                      hintText: "Enter Email",
+              
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.deepOrange),
+                      ),
                     ),
+              
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Entre Email";
+                      }
+                      return null;
+                    },
                   ),
-
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Entre Email";
-                    }
-                    return null;
-                  },
-                ),
-
-                SizedBox(height: 10),
-
-                TextFormField(
-                  obscureText: true,
-                  controller: password,
-
-                  decoration: InputDecoration(
-                    hintText: "Enter Password With six Digits",
-
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: Colors.deepOrange),
+              
+                  SizedBox(height: 10),
+              
+                  TextFormField(
+                    obscureText: true,
+                    controller: password,
+              
+                    decoration: InputDecoration(
+                      hintText: "Enter Password With six Digits",
+              
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.deepOrange),
+                      ),
                     ),
+              
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Entre Password";
+                      }
+                      return null;
+                    },
                   ),
-
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Entre Password";
-                    }
-                    return null;
-                  },
-                ),
-
-                SizedBox(height: 15),
-
-                ElevatedButton(
-                  onPressed: () {
-                 
-
-
-
-                    if (user.currentState!.validate()) {
-                        context.read<AuthBloc>().add(
-                      LoginEvent(email: email.text, password: password.text),
-                    ); 
-
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>Homescreen()));
-                    }
-                  },
-                  child: Text("Login"),
-                ),
-
-                SizedBox(height: 15),
-
-                Text("Not Have An Account ? ", style: TextStyle(fontSize: 13)),
-
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUpScreen()),
-                    );
-                  },
-                  child: Text("Sign Up", style: TextStyle(fontSize: 13)),
-                ),
-              ],
+              
+                  SizedBox(height: 15),
+              
+                  ElevatedButton(
+                    onPressed: () {
+                   
+              
+              
+              
+                      if (user.currentState!.validate()) {
+                          context.read<AuthBloc>().add(
+                        LoginEvent(email: email.text, password: password.text),
+                      ); 
+              
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>Homescreen()));
+                      }
+                    },
+                    child: Text("Login"),
+                  ),
+              
+                  SizedBox(height: 15),
+              
+                  Text("Not Have An Account ? ", style: TextStyle(fontSize: 13)),
+              
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpScreen()),
+                      );
+                    },
+                    child: Text("Sign Up", style: TextStyle(fontSize: 13)),
+                  ),
+                ],
+              ),
             ),
           );
         },
--- lib/Screens/sign_up_screen.dart
+++ lib/Screens/sign_up_screen.dart
@@ -1,11 +1,7 @@
-
-
-
 import 'package:chart_app/Blocs/Authbloc/bloc/auth_bloc.dart';
 import 'package:chart_app/Screens/homeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -15,94 +11,138 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final key = GlobalKey<FormState>();
 
-  final key=GlobalKey<FormState>();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController name = TextEditingController();
+  TextEditingController country = TextEditingController();
 
-TextEditingController email =TextEditingController();
-TextEditingController password=TextEditingController();
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign Up"),
-      ),
+      appBar: AppBar(title: Text("Sign Up")),
 
       body: Form(
-        key:key ,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-             Text("Sign Up", style: TextStyle(fontSize: 20)),
-        
-            SizedBox(height: 10),
-        
-            TextFormField(
-              obscureText: true,
+        key: key,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Sign Up", style: TextStyle(fontSize: 20)),
           
-              controller: email,
-        
-              decoration: InputDecoration(
-
-                hintText: "Enter Email",
-        
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(color: Colors.deepOrange),
+              SizedBox(height: 10),
+          
+              TextFormField(
+                obscureText: true,
+          
+                controller: name,
+          
+                decoration: InputDecoration(
+                  hintText: "Enter Name",
+          
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.deepOrange),
+                  ),
                 ),
+          
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Entre Name";
+                  }
+                  return null;
+                },
               ),
-        
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Entre Email";
-                }
-                return null;
-              },
-            ),
-        
-            SizedBox(height: 10),
-        
-            TextFormField(
-        
-              controller: password,
-        
-              decoration: InputDecoration(
-                hintText: "Enter Password With six Digits",
-        
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(color: Colors.deepOrange),
+          
+              SizedBox(height: 10),
+          
+              TextFormField(
+                controller: country,
+          
+                decoration: InputDecoration(
+                  hintText: "Enter Counutry",
+          
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.deepOrange),
+                  ),
                 ),
+          
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Entre Country";
+                  }
+                  return null;
+                },
               ),
-        
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Entre Password";
-                }
-                return null;
-              },
-            ),
-        
-             SizedBox(height: 15),
-        
-             ElevatedButton(onPressed: (){
-                 
-        
-                 if(key.currentState!.validate()){
-                   context.read<AuthBloc>().add(SigninEvent(email: email.text, password: password.text));
-
-               Navigator.push(context, MaterialPageRoute(builder: (context)=>Homescreen()));
-        
-                 }
-        
-        
-        
-             }, child: Text("Sign Up",style: TextStyle(fontSize: 15),)),
-        
-          ],
+          
+              SizedBox(height: 15),
+          
+              TextFormField(
+                controller: email,
+          
+                decoration: InputDecoration(
+                  hintText: "Enter Email",
+          
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.deepOrange),
+                  ),
+                ),
+          
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Entre Email";
+                  }
+                  return null;
+                },
+              ),
+          
+              SizedBox(height: 15),
+          
+              TextFormField(
+                controller: password,
+          
+                decoration: InputDecoration(
+                  hintText: "Enter Password With Six Digits",
+          
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.deepOrange),
+                  ),
+                ),
+          
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Entre Password";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+          
+              ElevatedButton(
+                onPressed: () {
+                  if (key.currentState!.validate()) {
+                    context.read<AuthBloc>().add(
+                      SigninEvent(email: email.text, password: password.text),
+                    );
+          
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Homescreen()),
+                    );
+                  }
+                },
+                child: Text("Sign Up", style: TextStyle(fontSize: 15)),
+              ),
+            ],
+          ),
         ),
       ),
     );
-}
+  }
 }
